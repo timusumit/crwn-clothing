5f3742f6700faf4f32276168f4ac566df4716bbe
Sumit Maharjan <timusumit@gmail.com>
2020-10-01 16:29:57 +0545

    sign in page done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () =>(
 <div>
@@ -21,6 +22,7 @@ function App() {
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUp} />
 
       </Switch>
     </div>
---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSingUpPage = () =>(
+    <div className='sign-in-and-sign-up'><SignIn/></div>
+
+)
+export default SignInAndSingUpPage;
