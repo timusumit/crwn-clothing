13508c8227423375bad13feaa40448017fb3edb8
Sumit Maharjan <timusumit@gmail.com>
2020-10-09 16:12:04 +0545

    user sign in with email done

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import HomePage from '../src/pages/homepage/homepage.component';
 import ShopPage from '../src/pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from './firebase/firebase.util.js';
+import {auth, createUserProfileDocument} from './firebase/firebase.util.js';
+
 
 
 
@@ -19,11 +20,21 @@ class  App extends React.Component {
   unsubscribeFromAuth =null
 
   componentDidMount(){
-   this.unsubscribeFromAuth= auth.onAuthStateChanged(user=>{
-      this.setState({
-        currentUser:user
-      });
-      console.log(user);
+   this.unsubscribeFromAuth= auth.onAuthStateChanged(async userAuth=>{
+     if(userAuth){
+       const userRef = await createUserProfileDocument(userAuth);
+       userRef.onSnapshot(snapShot=>{
+         this.setState({
+           currentUser:{
+             id:snapShot.id,
+             ...snapShot.data()
+           }
+         });
+         console.log(this.state);
+       });
+      
+     }
+     this.setState({currentUser:userAuth});
     });
 
   }
